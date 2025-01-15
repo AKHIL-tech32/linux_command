43eae616d2933452f10c585f32bc1f9432d837f8
akhil <akhildonge10@gmail.com>
2025-01-15 17:49:00 +0530

    my third commit

---
+++ star.java
@@ -0,0 +1,15 @@
+class Test {
+    public static void main (System [] args) 
+    {
+        for (int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++);
+            {
+              System.out.println(i);
+            }
+              System.out.print(" ");
+        }
+    }
+}
+
+
